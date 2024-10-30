d7e27a9df7215d9771a86d1b7affaca5cb7eb552
lokki931 <taurusr2d2@gmail.com>
2024-10-30 12:51:22 +0200

    contact page

--- app/contacts/page.js
+++ app/contacts/page.js
@@ -1,5 +1,46 @@
+import img from '@/app/contacts/_img/nostalgia-4397880_1280.jpg';
+import Image from 'next/image';
 const ContactsPage = () => {
-  return <div>ContactsPage</div>;
+  return (
+    <div>
+      <Image
+        className="float-left mr-6 mb-2 max-md:float-none"
+        src={img}
+        alt="Contact nostalgia image"
+        width={500}
+        height={300}
+      />
+      <p>
+        Lorem ipsum dolor sit amet consectetur, adipisicing elit. Iure sapiente quibusdam laboriosam
+        dignissimos voluptatibus dolorum molestiae illo vitae praesentium! Facilis qui dignissimos,
+        explicabo cumque saepe temporibus, placeat quaerat est culpa sed ducimus fugit consectetur
+        deleniti voluptas corporis debitis quis vero nihil? Pariatur, sint maiores dolores ab nemo
+        repudiandae id totam inventore atque, eius, delectus illum alias obcaecati hic? Magni quidem
+        reiciendis velit minus corrupti nisi aliquam, distinctio assumenda id nemo sit perspiciatis
+        voluptatum quibusdam laboriosam labore adipisci obcaecati quaerat. Dignissimos accusantium
+        nostrum consectetur. Cumque adipisci tenetur aspernatur enim nemo accusantium magni
+        necessitatibus quae voluptatibus. Excepturi amet aliquam architecto accusantium explicabo!
+        Lorem ipsum dolor sit amet consectetur, adipisicing elit. Iure sapiente quibusdam laboriosam
+        dignissimos voluptatibus dolorum molestiae illo vitae praesentium! Facilis qui dignissimos,
+        explicabo cumque saepe temporibus, placeat quaerat est culpa sed ducimus fugit consectetur
+        deleniti voluptas corporis debitis quis vero nihil? Pariatur, sint maiores dolores ab nemo
+        repudiandae id totam inventore atque, eius, delectus illum alias obcaecati hic? Magni quidem
+        reiciendis velit minus corrupti nisi aliquam, distinctio assumenda id nemo sit perspiciatis
+        voluptatum quibusdam laboriosam labore adipisci obcaecati quaerat. Dignissimos accusantium
+        nostrum consectetur. Cumque adipisci tenetur aspernatur enim nemo accusantium magni
+        necessitatibus quae voluptatibus. Excepturi amet aliquam architecto accusantium explicabo!
+        Lorem ipsum dolor sit amet consectetur, adipisicing elit. Iure sapiente quibusdam laboriosam
+        dignissimos voluptatibus dolorum molestiae illo vitae praesentium! Facilis qui dignissimos,
+        explicabo cumque saepe temporibus, placeat quaerat est culpa sed ducimus fugit consectetur
+        deleniti voluptas corporis debitis quis vero nihil? Pariatur, sint maiores dolores ab nemo
+        repudiandae id totam inventore atque, eius, delectus illum alias obcaecati hic? Magni quidem
+        reiciendis velit minus corrupti nisi aliquam, distinctio assumenda id nemo sit perspiciatis
+        voluptatum quibusdam laboriosam labore adipisci obcaecati quaerat. Dignissimos accusantium
+        nostrum consectetur. Cumque adipisci tenetur aspernatur enim nemo accusantium magni
+        necessitatibus quae voluptatibus. Excepturi amet aliquam architecto accusantium explicabo!
+      </p>
+    </div>
+  );
 };
 
 export default ContactsPage;
